71ca8cfa4ef088a0e3398052d84344809b335be1
円谷 雄二 <yuji.tsuburaya@bizreach.co.jp>
2018-09-28 16:50:34 +0900

    :+1: add animation sample

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
 // @flow
 
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { LayoutAnimation, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
@@ -33,22 +18,81 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  buttonArea: {
+    height: 120,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainArea: {
+    flex: 1,
+    padding: 24,
+  },
+  ball: {
+    backgroundColor: '#007aff',
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+  },
 });
 
+type State = {
+  ballNum: number,
+};
 type Props = {};
+type BallProps = {
+  style: Object,
+};
 
-export default class App extends Component<Props> {
+const Ball = (props: BallProps) => <View style={[styles.ball, props.style]} />;
+
+const CustomLayoutSpring = {
+  duration: 400,
+  create: {
+    type: LayoutAnimation.Types.spring,
+    property: LayoutAnimation.Properties.scaleXY,
+    springDamping: 0.7,
+  },
+  update: {
+    type: LayoutAnimation.Types.spring,
+    springDamping: 0.7,
+  },
+};
+
+export default class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = {};
+    this.state = {
+      ballNum: 3, // 初期描画用
+    };
   }
+
+  onPressAddButton = () => {
+    LayoutAnimation.configureNext(CustomLayoutSpring);
+    this.setState({ ballNum: this.state.ballNum + 1 });
+  };
+
+  onPressDeleteButton = () => {
+    if (this.state.ballNum === 0) return;
+    this.setState({ ballNum: this.state.ballNum - 1 });
+  };
+
+  renderBalls = () => {
+    const balls = [];
+    for (let i = 0; i < this.state.ballNum; i += 1) {
+      balls.push(<Ball key={i} style={{ marginBottom: 16 }} />);
+    }
+    return <View>{balls}</View>;
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.buttonArea}>
+          <Text onPress={this.onPressAddButton}>Add</Text>
+          <Text onPress={this.onPressDeleteButton}>Delete</Text>
+        </View>
+        <View style={styles.mainArea}>{this.renderBalls()}</View>
       </View>
     );
   }
