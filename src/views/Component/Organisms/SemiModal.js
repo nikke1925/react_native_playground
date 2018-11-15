8d872815245286fe0e12493649b97e2fa045807a
Yuji Tsuburaya 🌚 <yuji.tsuburaya@bizreach.co.jp>
2018-11-15 11:10:48 +0900

    :recycle: fix animation timing

--- src/views/Component/Organisms/SemiModal.js
+++ src/views/Component/Organisms/SemiModal.js
@@ -24,25 +24,10 @@ type State = {
 
 const MODAL_HEIGHT = 270;
 const MODAL_CLOSE_THRESHOLD = -40; // 閾値(40px下に動かしたら閉じる)
+const MODAL_BG_OPEN_DURATION = 50;
+const MODAL_BG_CLOSE_DURATION = 50;
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#FFF',
-    width: Dimensions.get('window').width - 32,
-    borderRadius: 16,
-    borderWidth: 0,
-    shadowColor: '#03091d',
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 1.0,
-    shadowRadius: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#151F2B',
-  },
   modal: {
     position: 'absolute',
     backgroundColor: '#151F2B',
@@ -54,15 +39,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     paddingTop: 8,
   },
-  modalWrapper: {
-    position: 'absolute',
-    backgroundColor: '#000000',
-    opacity: 0.64,
-    top: 0,
-    left: 0,
-    height: Dimensions.get('window').height,
-    width: '100%',
-  },
   modalBackground: {
     position: 'absolute',
     backgroundColor: '#00000066',
@@ -70,39 +46,6 @@ const styles = StyleSheet.create({
     left: 0,
     height: Dimensions.get('window').height,
     width: '100%',
-  },
-  modalText: {
-    color: '#FFF',
-  },
-  modalCancel: {
-    borderRadius: 32,
-    height: 32,
-    backgroundColor: '#243347',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalCancelButton: {
-    borderRadius: 32,
-    height: 40,
-    backgroundColor: '#243347',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  bar: {
-    width: 16,
-    borderBottomWidth: 4,
-    borderColor: '#FFFFFF44',
-  },
-  leftBar: {
-    borderRadius: 16,
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  rightBar: {
-    borderRadius: 16,
-    borderTopLeftRadius: 0,
-    borderBottomLeftRadius: 0,
   },
 });
 
@@ -155,7 +98,7 @@ export default class SemiModal extends Component<Props, State> {
       }),
       Animated.timing(this.state.modalBgPan, {
         toValue: { x: 0, y: 0 },
-        duration: 50,
+        duration: MODAL_BG_OPEN_DURATION,
         useNativeDriver: true,
       }),
     ]).start();
@@ -166,11 +109,13 @@ export default class SemiModal extends Component<Props, State> {
       Animated.spring(this.state.modalPan, {
         toValue: { x: 0, y: MODAL_HEIGHT * 2 },
         useNativeDriver: true,
+        duration: 0,
       }),
       Animated.timing(this.state.modalBgPan, {
         toValue: { x: 0, y: Dimensions.get('window').height },
-        duration: 50,
+        duration: MODAL_BG_CLOSE_DURATION,
         useNativeDriver: true,
+        delay: 150, // After modal animation
       }),
     ]).start(() => {
       this.props.onModalClose();
